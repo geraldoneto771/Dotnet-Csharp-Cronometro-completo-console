171f6258b2aa8e600b4272f2a69b18d78e7c6db8
geraldoneto771 <geraldopireslacerda771@gmail.com>
2022-09-22 19:34:32 -0300

    Opções do Menu

--- Program.cs
+++ Program.cs
@@ -15,12 +15,17 @@ namespace Stopwatch // Note: actual namespace depends on the project name.
             Console.Clear();
             Console.WriteLine("---------CRONÔMETRO--------");
             Console.WriteLine("---------------------------");
-            Console.WriteLine("S = Segundo");
-            Console.WriteLine("M = Minuto");
+            Console.WriteLine("S = Segundo => 10s =  10 segundos");
+            Console.WriteLine("M = Minuto => 1m = 1 minuto");
             Console.WriteLine("0 = Sair");
             Console.WriteLine("---------------------------");
             Console.WriteLine("Quanto tempo deseja contar?");
             //Console.WriteLine("---------------------------");
+
+            //Entrada de dados e convertendo para string em minusculo
+            string data = Console.ReadLine().ToLower();
+            //Console.WriteLine(data);
+
         }
 
         static void Start(int time)
